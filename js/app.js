13dedcaca71f1d55f0a7b2be99408550319795e8
Stefano Patti <mastepa@hotmail.com>
2021-04-27 14:45:57 +0200

    start prj

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function init() {
+     new Vue({
+          el: '#app',
+          data: {
+
+          },
+          
+          methods: {
+               
+          },
+     });
+     console.log('ciao prova');
+}
+
+
+document.addEventListener('DOMContentLoaded', init);
