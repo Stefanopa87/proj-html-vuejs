1d6a03e6b6ecbe4bdd3963dcd27ce31de1cfe70b
Stefano Patti <mastepa@hotmail.com>
2021-04-29 17:42:42 +0200

    prj done
     refactoring ok

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ function init() {
                     'PARTNERS',
                     'BLOG',
                ],
-
+               
                partnersImg:[
                     '-5',
                     '-1',
@@ -17,21 +17,56 @@ function init() {
                     '-3',
                     '-4',
                     '-5',
-               ]
-
+               ],
+               footerAbout:[
+                    'The Company',
+                    'Institutional',
+                    'Social & Events',
+                    'Innovation',
+                    'Environment',
+                    'Tecnology',
+               ],
+               footerTransport:[
+                    'Industrialized',
+                    'Chemicals',
+                    'Packaged Liquids',
+                    'Constuction',
+                    'Lamined Wood',
+                    'And others',
+               ],
+               footerSupport:[
+                    'Responsability',
+                    'Terms of Use',
+                    'About Cookies',
+                    'Privacy Policy',
+                    'Accessibility',
+                    'Information',
+               ],               
           },
           
-          methods: {
-               
+          methods: {             
+                                       
           },
-     });
-     console.log('VUE OK');
+          
+          mounted() {
+
+               var about = this.footerAbout
+               
+               console.log(about);
+
+               var transport = this.footerTransport
+               
+               console.log(transport);
+
+               var support = this.footerSupport
+               
+               console.log(support);
+          },          
+     });               
+     console.log('VUE OK');     
 }
 
 document.addEventListener('DOMContentLoaded', init);
 
-
-// jQuery per slider
-// $('.carousel').carousel()
 
 
